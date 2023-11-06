a0a10d1c06db06e38dd57e1243813250ff60a89d
Maxime Biechy <maxime.bch12@gmail.com>
2023-11-06 16:32:51 +0100

    structure code

--- Touiteur/index.php
+++ Touiteur/index.php
@@ -1,4 +1,12 @@
 <?php
 
 require_once('vendor/autoload.php');
-echo "test";
+
+use iutnc\touiteur\dispatch as dispatch;
+use iutnc\touiteur\db as db;
+
+session_start();
+db\ConnectionFactory::setConfig('src/classes/db/db.config.ini');
+
+$dispatcher = new dispatch\Dispatcher();
+$dispatcher->run();
--- Touiteur/src/classes/db/ConnectionFactory.php
+++ Touiteur/src/classes/db/ConnectionFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace iutnc\deefy\db;
+namespace iutnc\touiteur\db;
 
 use PDO;
 
