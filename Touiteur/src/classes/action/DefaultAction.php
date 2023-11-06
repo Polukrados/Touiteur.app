f331387af66932881c152daa5a6a4a8a034bcb3f
Polukrados <mathieu.dziezuk@gmail.com>
2023-11-06 17:30:16 +0100

    t

--- Touiteur/src/classes/action/DefaultAction.php
+++ Touiteur/src/classes/action/DefaultAction.php
@@ -3,12 +3,10 @@
 namespace iutnc\touiteur\action;
 
 use iutnc\touiteur\action\Action;
+use iutnc\touiteur\db\ConnectionFactory;
 
 class DefaultAction extends Action
 {
-
-    private array $listTouit;
-
     public function __construct()
     {
         parent::__construct();
@@ -16,18 +14,42 @@ class DefaultAction extends Action
 
     public function execute(): string
     {
-        $res="";
-        $res.=<<<HTML
+        $db = ConnectionFactory::makeConnection();
+        $query = $db->query("SELECT * FROM tweets ORDER BY timestamp DESC");
+        $tweets = '';
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $user_id = $row['user_id']; // Supposons que vous avez une colonne "user_id" pour l'identifiant de l'utilisateur
+            $content = $row['content'];
+            $timestamp = $row['timestamp'];
+
+            // Tweet
+            $tweetHTML = <<<HTML
+                <div class="tweet">
+                    <div class="user">User ID: $user_id</div>
+                    <div class="content">$content</div>
+                    <div class="timestamp">Posted at: $timestamp</div>
+                </div>
+            HTML;
+
+            $tweets .= $tweetHTML;
+        }
+
+        // Page
+        $pageContent = <<<HTML
             <header> 
-              <p class ='libelle_page_courante'>Accueil</p> 
-              <nav class="menu">
-                 <ul>
-                  <li><a href="?action=add-user">S'inscrire</a></li>
-                  <li><a href="?action=signin">Se connecter</a></li>
-                 </ul>
-               </nav>
+                <p class="libelle_page_courante">Accueil</p> 
+                <nav class="menu">
+                    <ul>
+                        <li><a href="?action=accueil">S'inscrire</a></li>
+                        <li><a href="?action=accueil">Se connecter</a></li>
+                    </ul>
+                </nav>
             </header>
-           HTML;
-        return $res;
+            <div class="tweets">
+                $tweets
+            </div>
+        HTML;
+
+        return $pageContent;
     }
 }
--- Touiteur/src/classes/action/Touit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace iutnc\touiteur\action;
-
-class Touit{
-    private User $user;
-    private string $description;
-    private ?string $image=null;
-}
