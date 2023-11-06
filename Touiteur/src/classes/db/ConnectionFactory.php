b521273e47c18da0d5f292bd99982b0be1735d42
Maxime Biechy <maxime.bch12@gmail.com>
2023-11-06 15:50:09 +0100

    structure classes

---
+++ Touiteur/src/classes/db/ConnectionFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace iutnc\deefy\db;
+
+use PDO;
+
+class ConnectionFactory
+{
+
+    private static $tableau = [];
+
+    public static function setConfig($file)
+    {
+        self::$tableau = parse_ini_file($file);
+    }
+
+    public static function makeConnection(): PDO
+    {
+        $dns = self::$tableau['driver'] . ":host=" . self::$tableau['host'] . ";dbname=" . self::$tableau['database'];
+        $username = self::$tableau['username'];
+        $password = self::$tableau['password'];
+
+        $db = new PDO($dns, $username, $password, [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+        ]);
+
+        $db->prepare('SET NAMES \'UTF8\'')->execute();
+
+        return $db;
+    }
+}
