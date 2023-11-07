d1b0611d01ff7d8e02372156508549abb5324a44
Polukrados <mathieu.dziezuk@gmail.com>
2023-11-07 10:27:03 +0100

    Touite mieux

--- Touiteur/bd/touiteur_app.sql
+++ Touiteur/bd/touiteur_app.sql
@@ -28,19 +28,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `abonnementtags` (
-  `userID` int(11) NOT NULL,
-  `tagID` int(11) NOT NULL
+                                  `utilisateurID` int(11) NOT NULL,
+                                  `tagID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `abonnementtags`
 --
 
-INSERT INTO `abonnementtags` (`userID`, `tagID`) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
+INSERT INTO `abonnementtags` (`utilisateurID`, `tagID`) VALUES
+                                                            (1, 1),
+                                                            (2, 2),
+                                                            (3, 3),
+                                                            (4, 4);
 
 -- --------------------------------------------------------
 
@@ -49,20 +49,20 @@ INSERT INTO `abonnementtags` (`userID`, `tagID`) VALUES
 --
 
 CREATE TABLE `evaluations` (
-  `touiteID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `note` int(11) DEFAULT NULL
+                               `touiteID` int(11) NOT NULL,
+                               `utilisateurID` int(11) NOT NULL,
+                               `note` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `evaluations`
 --
 
-INSERT INTO `evaluations` (`touiteID`, `userID`, `note`) VALUES
-(1, 2, 8),
-(1, 3, 7),
-(2, 1, 5),
-(3, 4, 9);
+INSERT INTO `evaluations` (`touiteID`, `utilisateurID`, `note`) VALUES
+                                                                    (1, 2, 8),
+                                                                    (1, 3, 7),
+                                                                    (2, 1, 5),
+                                                                    (3, 4, 9);
 
 -- --------------------------------------------------------
 
@@ -71,9 +71,9 @@ INSERT INTO `evaluations` (`touiteID`, `userID`, `note`) VALUES
 --
 
 CREATE TABLE `images` (
-  `imageID` int(11) NOT NULL,
-  `description` text DEFAULT NULL,
-  `cheminFichier` varchar(255) DEFAULT NULL
+                          `imageID` int(11) NOT NULL,
+                          `description` text DEFAULT NULL,
+                          `cheminFichier` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -81,10 +81,10 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`imageID`, `description`, `cheminFichier`) VALUES
-(1, 'Image de profil', 'C:\xampp\htdocs\Touiteur.app\Touiteur\images\icone_de_profil.jpg'),
-(2, 'Couverture pour touite', 'C:\xampp\htdocs\Touiteur.app\Touiteur\images\couverture_touite.jpg'),
-(3, 'Illustration de touite', 'C:\xampp\htdocs\Touiteur.app\Touiteur\images\illustration_touite.jpg'),
-(4, 'Photo de paysage', 'C:\xampp\htdocs\Touiteur.app\Touiteur\images\photo_paysage.jpg');
+                                                                     (1, 'Image de profil', 'icone_de_profil.jpg'),
+                                                                     (2, 'Couverture pour touite', 'couverture_touite.jpg'),
+                                                                     (3, 'Illustration de touite', 'illustration_touite.jpg'),
+                                                                     (4, 'Photo de paysage', 'photo_paysage.jpg');
 
 -- --------------------------------------------------------
 
@@ -93,19 +93,19 @@ INSERT INTO `images` (`imageID`, `description`, `cheminFichier`) VALUES
 --
 
 CREATE TABLE `suivi` (
-  `followerID` int(11) NOT NULL,
-  `followedID` int(11) NOT NULL
+                         `suivreID` int(11) NOT NULL,
+                         `suiviID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `suivi`
 --
 
-INSERT INTO `suivi` (`followerID`, `followedID`) VALUES
-(1, 2),
-(1, 3),
-(2, 3),
-(3, 4);
+INSERT INTO `suivi` (`suivreID`, `suiviID`) VALUES
+                                                (1, 2),
+                                                (1, 3),
+                                                (2, 3),
+                                                (3, 4);
 
 -- --------------------------------------------------------
 
@@ -114,9 +114,9 @@ INSERT INTO `suivi` (`followerID`, `followedID`) VALUES
 --
 
 CREATE TABLE `tags` (
-  `tagID` int(11) NOT NULL,
-  `libelle` varchar(100) DEFAULT NULL,
-  `description` text DEFAULT NULL
+                        `tagID` int(11) NOT NULL,
+                        `libelle` varchar(100) DEFAULT NULL,
+                        `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -124,10 +124,10 @@ CREATE TABLE `tags` (
 --
 
 INSERT INTO `tags` (`tagID`, `libelle`, `description`) VALUES
-(1, '#ExempleTag', 'Un tag pour des exemples'),
-(2, '#Portfolio', 'Tag lié aux portfolios'),
-(3, '#Accord', 'Tag pour exprimer l’accord'),
-(4, '#Touiteur', 'Le tag officiel de Touiteur');
+                                                           (1, '#ExempleTag', 'Un tag pour des exemples'),
+                                                           (2, '#Portfolio', 'Tag lié aux portfolios'),
+                                                           (3, '#Accord', 'Tag pour exprimer l’accord'),
+                                                           (4, '#Touiteur', 'Le tag officiel de Touiteur');
 
 -- --------------------------------------------------------
 
@@ -136,21 +136,20 @@ INSERT INTO `tags` (`tagID`, `libelle`, `description`) VALUES
 --
 
 CREATE TABLE `touites` (
-  `touiteID` int(11) NOT NULL,
-  `texte` varchar(235) DEFAULT NULL,
-  `userID` int(11) DEFAULT NULL,
-  `datePublication` datetime DEFAULT NULL
+                           `touiteID` int(11) NOT NULL,
+                           `texte` varchar(235) DEFAULT NULL,
+                           `datePublication` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `touites`
 --
 
-INSERT INTO `touites` (`touiteID`, `texte`, `userID`, `datePublication`) VALUES
-(1, 'Ceci est mon premier touite !', 1, '2023-11-06 10:00:00'),
-(2, 'Un autre touite avec un #ExempleTag', 2, '2023-11-06 11:00:00'),
-(3, 'Découvrez mon travail sur mon site #Portfolio', 3, '2023-11-06 12:00:00'),
-(4, 'Je suis d’accord avec ça #Accord', 4, '2023-11-06 13:00:00');
+INSERT INTO `touites` (`touiteID`, `texte`, `datePublication`) VALUES
+                                                                   (1, 'Ceci est mon premier touite !', '2023-11-06 10:00:00'),
+                                                                   (2, 'Un autre touite avec un #ExempleTag', '2023-11-06 11:00:00'),
+                                                                   (3, 'Découvrez mon travail sur mon site #Portfolio', '2023-11-06 12:00:00'),
+                                                                   (4, 'Je suis d’accord avec ça #Accord', '2023-11-06 13:00:00');
 
 -- --------------------------------------------------------
 
@@ -159,8 +158,8 @@ INSERT INTO `touites` (`touiteID`, `texte`, `userID`, `datePublication`) VALUES
 --
 
 CREATE TABLE `touitesimages` (
-  `TouiteID` int(11) NOT NULL,
-  `ImageID` int(11) NOT NULL
+                                 `TouiteID` int(11) NOT NULL,
+                                 `ImageID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -168,8 +167,8 @@ CREATE TABLE `touitesimages` (
 --
 
 INSERT INTO `touitesimages` (`TouiteID`, `ImageID`) VALUES
-(2, 2),
-(3, 3);
+                                                        (2, 2),
+                                                        (3, 3);
 
 -- --------------------------------------------------------
 
@@ -178,8 +177,8 @@ INSERT INTO `touitesimages` (`TouiteID`, `ImageID`) VALUES
 --
 
 CREATE TABLE `touitestags` (
-  `TouiteID` int(11) NOT NULL,
-  `TagID` int(11) NOT NULL
+                               `TouiteID` int(11) NOT NULL,
+                               `TagID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -187,9 +186,29 @@ CREATE TABLE `touitestags` (
 --
 
 INSERT INTO `touitestags` (`TouiteID`, `TagID`) VALUES
-(2, 1),
-(3, 2),
-(4, 3);
+                                                    (2, 1),
+                                                    (3, 2),
+                                                    (4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `touitesutilisateurs`
+--
+
+CREATE TABLE `touitesutilisateurs` (
+                                       `TouiteID` int(11) NOT NULL,
+                                       `utilisateurID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Déchargement des données de la table `touitesutilisateurs`
+--
+
+INSERT INTO `touitesutilisateurs` (`TouiteID`, `utilisateurID`) VALUES
+                                                                    (1, 4),
+                                                                    (4, 1),
+                                                                    (3, 2);
 
 -- --------------------------------------------------------
 
@@ -198,21 +217,22 @@ INSERT INTO `touitestags` (`TouiteID`, `TagID`) VALUES
 --
 
 CREATE TABLE `utilisateurs` (
-  `userID` int(11) NOT NULL,
-  `nom` varchar(255) DEFAULT NULL,
-  `prenom` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL
+                                `utilisateurID` int(11) NOT NULL,
+                                `nom` varchar(255) DEFAULT NULL,
+                                `prenom` varchar(255) DEFAULT NULL,
+                                `email` varchar(255) DEFAULT NULL,
+                                `mdp` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`userID`, `nom`, `prenom`, `email`) VALUES
-(1, 'Dupont', 'Alice', 'alice.dupont@gmail.com'),
-(2, 'Durand', 'Bob', 'bob.durand@hotmail.com'),
-(3, 'Leroy', 'Charlie', 'charlie.leroy@sfr.com'),
-(4, 'Moreau', 'Diana', 'diana.moreau@gmail.com');
+INSERT INTO `utilisateurs` (`utilisateurID`, `nom`, `prenom`, `email`, `mdp`) VALUES
+                                                                                  (1, 'Dupont', 'Alice', 'alice.dupont@gmail.com', 'jaimemonchat'),
+                                                                                  (2, 'Durand', 'Bob', 'bob.durand@hotmail.com', 'durandbob'),
+                                                                                  (3, 'Leroy', 'Charlie', 'charlie.leroy@sfr.com', 'azertyuiop'),
+                                                                                  (4, 'Moreau', 'Diana', 'diana.moreau@gmail.com', 'diana357');
 
 --
 -- Index pour les tables déchargées
@@ -222,61 +242,73 @@ INSERT INTO `utilisateurs` (`userID`, `nom`, `prenom`, `email`) VALUES
 -- Index pour la table `abonnementtags`
 --
 ALTER TABLE `abonnementtags`
-  ADD PRIMARY KEY (`userID`,`tagID`),
+    ADD PRIMARY KEY (`utilisateurID`,`tagID`),
+  ADD KEY `utilisateurID` (`utilisateurID`),
   ADD KEY `tagID` (`tagID`);
 
 --
 -- Index pour la table `evaluations`
 --
 ALTER TABLE `evaluations`
-  ADD PRIMARY KEY (`touiteID`,`userID`),
-  ADD KEY `userID` (`userID`);
+    ADD PRIMARY KEY (`touiteID`,`utilisateurID`),
+  ADD KEY `touiteID` (`touiteID`),
+  ADD KEY `utilisateurID` (`utilisateurID`);
 
 --
 -- Index pour la table `images`
 --
 ALTER TABLE `images`
-  ADD PRIMARY KEY (`imageID`);
+    ADD PRIMARY KEY (`imageID`);
 
 --
 -- Index pour la table `suivi`
 --
 ALTER TABLE `suivi`
-  ADD PRIMARY KEY (`followerID`,`followedID`),
-  ADD KEY `followedID` (`followedID`);
+    ADD PRIMARY KEY (`suivreID`,`suiviID`),
+  ADD KEY `suivreID` (`suivreID`),
+  ADD KEY `suiviID` (`suiviID`);
 
 --
 -- Index pour la table `tags`
 --
 ALTER TABLE `tags`
-  ADD PRIMARY KEY (`tagID`);
+    ADD PRIMARY KEY (`tagID`);
 
 --
 -- Index pour la table `touites`
 --
 ALTER TABLE `touites`
-  ADD PRIMARY KEY (`touiteID`),
-  ADD KEY `userID` (`userID`);
+    ADD PRIMARY KEY (`touiteID`);
 
 --
 -- Index pour la table `touitesimages`
 --
 ALTER TABLE `touitesimages`
-  ADD PRIMARY KEY (`TouiteID`,`ImageID`),
+    ADD PRIMARY KEY (`TouiteID`,`ImageID`),
+  ADD KEY `TouiteID` (`TouiteID`),
   ADD KEY `ImageID` (`ImageID`);
 
 --
 -- Index pour la table `touitestags`
 --
 ALTER TABLE `touitestags`
-  ADD PRIMARY KEY (`TouiteID`,`TagID`),
+    ADD PRIMARY KEY (`TouiteID`,`TagID`),
+  ADD KEY `TouiteID` (`TouiteID`),
   ADD KEY `TagID` (`TagID`);
+
+--
+-- Index pour la table `touitesutilisateurs`
+--
+ALTER TABLE `touitesutilisateurs`
+    ADD PRIMARY KEY (`TouiteID`,`utilisateurID`),
+  ADD KEY `TouiteID` (`TouiteID`),
+  ADD KEY `TagID` (`utilisateurID`);
 
 --
 -- Index pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  ADD PRIMARY KEY (`userID`);
+    ADD PRIMARY KEY (`utilisateurID`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -286,25 +318,25 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `images`
 --
 ALTER TABLE `images`
-  MODIFY `imageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `imageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `tagID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `tagID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `touites`
 --
 ALTER TABLE `touites`
-  MODIFY `touiteID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `touiteID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `utilisateurID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Contraintes pour les tables déchargées
@@ -314,42 +346,43 @@ ALTER TABLE `utilisateurs`
 -- Contraintes pour la table `abonnementtags`
 --
 ALTER TABLE `abonnementtags`
-  ADD CONSTRAINT `abonnementtags_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `utilisateurs` (`userID`),
+    ADD CONSTRAINT `abonnementtags_ibfk_1` FOREIGN KEY (`utilisateurID`) REFERENCES `utilisateurs` (`utilisateurID`),
   ADD CONSTRAINT `abonnementtags_ibfk_2` FOREIGN KEY (`tagID`) REFERENCES `tags` (`tagID`);
 
 --
 -- Contraintes pour la table `evaluations`
 --
 ALTER TABLE `evaluations`
-  ADD CONSTRAINT `evaluations_ibfk_1` FOREIGN KEY (`touiteID`) REFERENCES `touites` (`touiteID`),
-  ADD CONSTRAINT `evaluations_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `utilisateurs` (`userID`);
+    ADD CONSTRAINT `evaluations_ibfk_1` FOREIGN KEY (`touiteID`) REFERENCES `touites` (`touiteID`),
+  ADD CONSTRAINT `evaluations_ibfk_2` FOREIGN KEY (`utilisateurID`) REFERENCES `utilisateurs` (`utilisateurID`);
 
 --
 -- Contraintes pour la table `suivi`
 --
 ALTER TABLE `suivi`
-  ADD CONSTRAINT `suivi_ibfk_1` FOREIGN KEY (`followerID`) REFERENCES `utilisateurs` (`userID`),
-  ADD CONSTRAINT `suivi_ibfk_2` FOREIGN KEY (`followedID`) REFERENCES `utilisateurs` (`userID`);
-
---
--- Contraintes pour la table `touites`
---
-ALTER TABLE `touites`
-  ADD CONSTRAINT `touites_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `utilisateurs` (`userID`);
+    ADD CONSTRAINT `suivi_ibfk_1` FOREIGN KEY (`suivreID`) REFERENCES `utilisateurs` (`utilisateurID`),
+  ADD CONSTRAINT `suivi_ibfk_2` FOREIGN KEY (`suiviID`) REFERENCES `utilisateurs` (`utilisateurID`);
 
 --
 -- Contraintes pour la table `touitesimages`
 --
 ALTER TABLE `touitesimages`
-  ADD CONSTRAINT `touitesimages_ibfk_1` FOREIGN KEY (`TouiteID`) REFERENCES `touites` (`touiteID`),
+    ADD CONSTRAINT `touitesimages_ibfk_1` FOREIGN KEY (`TouiteID`) REFERENCES `touites` (`touiteID`),
   ADD CONSTRAINT `touitesimages_ibfk_2` FOREIGN KEY (`ImageID`) REFERENCES `images` (`imageID`);
 
 --
 -- Contraintes pour la table `touitestags`
 --
 ALTER TABLE `touitestags`
-  ADD CONSTRAINT `touitestags_ibfk_1` FOREIGN KEY (`TouiteID`) REFERENCES `touites` (`touiteID`),
+    ADD CONSTRAINT `touitestags_ibfk_1` FOREIGN KEY (`TouiteID`) REFERENCES `touites` (`touiteID`),
   ADD CONSTRAINT `touitestags_ibfk_2` FOREIGN KEY (`TagID`) REFERENCES `tags` (`tagID`);
+
+--
+-- Contraintes pour la table `touitesutilisateurs`
+--
+ALTER TABLE `touitesutilisateurs`
+    ADD CONSTRAINT `touitesutilisateurs_ibfk_1` FOREIGN KEY (`TouiteID`) REFERENCES `touites` (`touiteID`),
+  ADD CONSTRAINT `touitesutilisateurs_ibfk_2` FOREIGN KEY (`utilisateurID`) REFERENCES `utilisateurs` (`utilisateurID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Touiteur/src/classes/action/DefaultAction.php
+++ Touiteur/src/classes/action/DefaultAction.php
@@ -16,49 +16,104 @@ class DefaultAction extends Action
     public function execute(): string
     {
         $db = ConnectionFactory::makeConnection();
-        $query = $db->query("SELECT Touites.texte, Utilisateurs.nom, Utilisateurs.prenom, Touites.datePublication, Images.cheminFichier
-                            FROM Touites
-                            INNER JOIN Utilisateurs ON Touites.userID = Utilisateurs.userID
-                            LEFT JOIN TouitesImages ON Touites.touiteID = TouitesImages.touiteID
-                            LEFT JOIN Images ON TouitesImages.imageID = Images.imageID
-                            ORDER BY Touites.datePublication DESC");
 
-        $tweets = '';
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $userName = $row['prenom'] . ' ' . $row['nom'];
-            $content = $row['texte'];
-            $timestamp = $row['datePublication'];
-            $imagePath = $row['cheminFichier'];
+        if ($this->http_method === 'GET') {
+            $query = $db->query("SELECT Touites.touiteID, Touites.texte, Utilisateurs.nom, Utilisateurs.prenom, Touites.datePublication
+                        FROM Touites
+                        INNER JOIN TouitesUtilisateurs ON Touites.touiteID = TouitesUtilisateurs.TouiteID
+                        INNER JOIN Utilisateurs ON TouitesUtilisateurs.utilisateurID = Utilisateurs.utilisateurID
+                        ORDER BY Touites.datePublication DESC");
 
-            // Tweet
-            $tweetHTML = <<<HTML
-                <div class="tweet">
-                    <div class="user">Utilisateur: $userName</div>
-                    <div class="content">$content</div>
-                    <div class="timestamp">Publié le : $timestamp</div>
-                    <img src="$imagePath" alt="Image associée au tweet">
-                </div>
-            HTML;
+            $tweets = '';
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $tweetID = $row['touiteID'];
+                $userName = $row['prenom'] . ' ' . $row['nom'];
+                $content = $row['texte'];
+                $timestamp = $row['datePublication'];
 
-            $tweets .= $tweetHTML;
-        }
-
-        // Page
-        $pageContent = <<<HTML
-            <header> 
-                <p class="libelle_page_courante">Accueil</p> 
-                <nav class="menu">
-                    <ul>
-                        <li><a href="?action=accueil">S'inscrire</a></li>
-                        <li><a href="?action=accueil">Se connecter</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <div class="tweets">
-                $tweets
+                // Tweet court avec un formulaire pour les détails
+                $tweetHTML = <<<HTML
+            <div class="tweet">
+                <div class="user">Utilisateur: $userName</div>
+                <div class="content">$content</div>
+                <div class="timestamp">Publié le : $timestamp</div>
+                <form method="post" action="?action=default">
+                    <input type="hidden" name="tweet_id" value="$tweetID">
+                    <input type="submit" value="Voir les détails">
+                </form>
             </div>
         HTML;
 
-        return $pageContent;
+                $tweets .= $tweetHTML;
+            }
+
+            // Page
+            $pageContent = <<<HTML
+        <header> 
+            <p class="libelle_page_courante">Accueil</p> 
+            <nav class="menu">
+                <ul>
+                    <li><a href="?action=add-user">S'inscrire</a></li>
+                    <li><a href="?action=signin">Se connecter</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="tweets">
+            $tweets
+        </div>
+    HTML;
+
+            return $pageContent;
+        } else {
+            if (isset($_POST['tweet_id'])) {
+                $tweetID = intval($_POST['tweet_id']);
+                $query = $db->prepare("SELECT Touites.texte, Utilisateurs.nom, Utilisateurs.prenom, Touites.datePublication, Evaluations.note
+                        FROM Touites
+                        INNER JOIN TouitesUtilisateurs ON Touites.touiteID = TouitesUtilisateurs.TouiteID
+                        INNER JOIN Utilisateurs ON TouitesUtilisateurs.utilisateurID = Utilisateurs.utilisateurID
+                        LEFT JOIN Evaluations ON Touites.touiteID = Evaluations.touiteID
+                        WHERE Touites.touiteID = :tweet_id");
+                $query->bindParam(':tweet_id', $tweetID, PDO::PARAM_INT);
+                $query->execute();
+
+                $row = $query->fetch(PDO::FETCH_ASSOC);
+                if ($row) {
+                    $userName = $row['prenom'] . ' ' . $row['nom'];
+                    $content = $row['texte'];
+                    $timestamp = $row['datePublication'];
+                    $score = $row['note'] ?? 'N/A'; // Si la note n'est pas disponible
+
+                    // Tweet en détail
+                    $tweetHTML = <<<HTML
+                <div class="tweet-detail">
+                    <div class="user">Utilisateur: $userName</div>
+                    <div class="content">$content</div>
+                    <div class="timestamp">Publié le : $timestamp</div>
+                    <div class="score">Score : $score</div>
+                </div>
+            HTML;
+
+                    // Lien pour retourner à la liste des tweets
+                    $backLink = '<a href="?action=default">Retour à la liste des tweets</a>';
+
+                    // Page
+                    $pageContent = <<<HTML
+                <header> 
+                    <p class="libelle_page_courante">Détails du Tweet</p>
+                </header>
+                <div class="tweet-details">
+                    $tweetHTML
+                    $backLink
+                </div>
+            HTML;
+
+                    return $pageContent;
+                } else {
+                    return 'Tweet non trouvé.';
+                }
+            } else {
+                return 'ID';
+            }
+        }
     }
 }
