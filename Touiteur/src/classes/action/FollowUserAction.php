0d84ba4172f83ed143c7b503add370983a76c905
CocoDietrich <corentindietrich57@gmail.com>
2023-11-08 10:41:46 +0100

    "Fonctionnalité 13"

--- Touiteur/src/classes/action/FollowUserAction.php
+++ Touiteur/src/classes/action/FollowUserAction.php
@@ -89,25 +89,20 @@ class FollowUserAction extends Action
 
         if ($this->http_method === 'GET') {
             // Vérifie si un utilisateur a été sélectionné
-                $follow = $db->prepare('SELECT suiviID FROM suivi WHERE suivreID = :followID');
-                                            $follow->bindParam(':followID', $_SESSION['utilisateur']['userID']);
-                                            $follow->execute();
-                                            $following = false;
-                                            while($res = $follow->fetch(PDO::FETCH_ASSOC)) {
-                                                if($res == $userID) {
-                                                    $following = true;
-                                                    break;
-                                                }
-                                            }
+           $checkQuery = $db->prepare("SELECT COUNT(*) FROM suivi WHERE suivreID = :followID AND suiviID = :followedID");
+           $checkQuery->bindParam(':followID', $_SESSION['utilisateur']['userID'], PDO::PARAM_INT);
+           $checkQuery->bindParam(':followedID', $userID, PDO::PARAM_INT);
+           $checkQuery->execute();
+           $existingRelation = $checkQuery->fetchColumn();
 
-                                            if(!$following) {
+                                            if($existingRelation===0) {
                                                 $pageContent.= <<<HTML
                                                 <div class="profil">
                                                                     <h3>Nom : $nom</h3>
                                                                     <h3>Prénom : $prenom</h3>
                                                                     <form method="post">
                                                                         <input type="hidden" name="user_id" value="$userID">
-                                                                        <input type="submit" value="Suivre cet utilisateur (retourne à l'accueil)">
+                                                                        <input type="submit" value="Suivre cet utilisateur">
                                                                     </form>
                                                                     </div>
                                                                     <div class="tweets">
@@ -122,7 +117,7 @@ class FollowUserAction extends Action
                                                 <div class="profil">
                                                                     <h3>Nom : $nom</h3>
                                                                     <h3>Prénom : $prenom</h3>
-                                                                    <p>Vous suivez déjà cet utilisateur.</p>
+                                                                    <p>Vous suivez cet utilisateur.</p>
                                                                     </div>
                                                                     <div class="tweets">
                                                                     <br>
@@ -139,27 +134,6 @@ class FollowUserAction extends Action
             $pageContent = '';
 
             if ($userID !== null) {
-                $checkQuery = $db->prepare("SELECT COUNT(*) FROM suivi WHERE suivreID = :followID AND suiviID = :followedID");
-                $checkQuery->bindParam(':followID', $_SESSION['utilisateur']['userID'], PDO::PARAM_INT);
-                $checkQuery->bindParam(':followedID', $userID, PDO::PARAM_INT);
-                $checkQuery->execute();
-                $existingRelation = $checkQuery->fetchColumn();
-
-                if ($existingRelation != 0) {
-                    $pageContent = <<<HTML
-                                                            <header>
-                                                                <p class="libelle_page_courante">Profil de l'utilisateur : $userName</p>
-                                                                <nav class="menu">
-                                                                    <ul>
-                                                                        <li><a href="?action=add-user">S'inscrire</a></li>
-                                                                        <li><a href="?action=signin">Se connecter</a></li>
-                                                                        <li><a href="?action=default">Retour à l'accueil</a></li>
-                                                                    </ul>
-                                                                </nav>
-                                                            </header>
-                                                            <p> Vous suivez déjà cet utilisateur !</p>
-                    HTML;
-                } else {
                     // La relation n'existe pas, vous pouvez effectuer l'insertion.
                     $insertQuery = $db->prepare("INSERT INTO suivi(suivreID, suiviID) VALUES(:followID, :followedID)");
                     $insertQuery->bindParam(':followID', $_SESSION['utilisateur']['userID'], PDO::PARAM_INT);
@@ -178,8 +152,7 @@ class FollowUserAction extends Action
                                                                 </nav>
                                                             </header>
                                                             <p> Vous suivez maintenant l'utilisateur $userName !</p>
-                                HTML;
-                }
+                    HTML;
             }
 
             return $pageContent;
